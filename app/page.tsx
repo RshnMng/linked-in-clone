cb9fb6df463f6c2fffedf4ad53181c73966e3293
rashaan <rmng.developer@gmail.com>
2025-07-31 10:46:19 -0400

    began user information section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import UserInformation from "@/components/UserInformation";
+
 export default function Home() {
   return (
-    <div className="flex justify-between p-4 bg-white shadow-md">
-      linked in clone
+    <div className="grid">
+      <section>
+        {/* user information */}
+        <UserInformation />
+      </section>
+      <section>
+        {/* post form  */}
+        {/* post feed */}
+      </section>
+      <section>{/* widget */}</section>
     </div>
   );
 }
