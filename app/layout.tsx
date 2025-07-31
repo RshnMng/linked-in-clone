98cc71979ebffbba202465200a3cc4a395faabbb
rashaan <rmng.developer@gmail.com>
2025-07-31 10:02:52 -0400

    finished  main header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "LinkedIn Clone",
@@ -13,14 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <header>
-          <Header />
-        </header>
-
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen flex flex-col">
+          <header className="border-b sticky top-0 bg-white z-50">
+            <Header />
+          </header>
+          <div className="bg-[#F4F2ED] flex-1 w-full">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,14 @@
-import { MenuIcon, SearchIcon } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  MenuIcon,
+  SearchIcon,
+  UsersIcon,
+  Briefcase,
+  MessagesSquare,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
 //
 //
 function Header() {
@@ -14,7 +22,7 @@ function Header() {
         alt="image"
       />
 
-      <div className="flex-1">
+      <div className="flex-1 ml-3">
         <form className="flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 max-w-96">
           <SearchIcon className="h-4 text-gray-600" />
           <input
@@ -25,12 +33,40 @@ function Header() {
         </form>
       </div>
 
-      <div>
+      <div className="flex items-center space-x-4 px-6">
         <Link href="/" className="icon">
           <MenuIcon className="h-5" />
+          <p>Name</p>
         </Link>
-        <Link href="/"></Link>
-        <Link href="/"></Link>
+        <Link href="/" className="icon hidden md:flex">
+          <UsersIcon className="h-5" />
+          <p>Network</p>
+        </Link>
+        <Link href="/" className="icon hidden md:flex">
+          <Briefcase className="h-5" />
+          <p>Jobs</p>
+        </Link>
+
+        <Link href="/" className="icon">
+          <MessagesSquare className="h-5" />
+          <p>Messaging</p>
+        </Link>
+
+        {/* User button */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        {/* Sign in button if not signed in */}
+        <SignedOut>
+          <Button
+            asChild
+            variant="outline"
+            className="bg-blue-600 text-white font-bold"
+          >
+            <SignInButton />
+          </Button>
+        </SignedOut>
       </div>
     </div>
   );
