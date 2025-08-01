f290676274b9d7bfcb5ac056b6a7b73fc7581c74
rashaan <rmng.developer@gmail.com>
2025-08-01 09:45:49 -0400

    created preview image feature

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -3,9 +3,9 @@
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { useUser } from "@clerk/nextjs";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, XIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 //
 
@@ -13,49 +13,81 @@ function PostForm() {
   const { user, isLoaded } = useUser();
   const ref = useRef<HTMLFormElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [preview, setPreview] = useState<string | null>(null);
 
   if (!isLoaded) return null;
 
   const firstName = user?.firstName;
   const lastName = user?.lastName;
   const imageUrl = user?.imageUrl;
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    }
+  };
   return (
-    <div>
-      <div>
-        <form action="">
-          <div className="flex items-center space-x-2">
-            <Avatar>
-              <AvatarImage src={imageUrl} />
-              <AvatarFallback>
-                {firstName?.charAt(0)}
-                {lastName?.charAt(0)}
-              </AvatarFallback>
-            </Avatar>
+    <div className="mb-2">
+      <form action="" ref={ref} className="p-3 bg-white rounded-lg border">
+        <div className="flex items-center space-x-2">
+          <Avatar>
+            <AvatarImage src={imageUrl} />
+            <AvatarFallback>
+              {firstName?.charAt(0)}
+              {lastName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
 
-            <input
-              type="text"
-              name="postInput"
-              placeholder="Start writing a post..."
-              className="flex-1 outline-none rounded-full py-3 px-4 border"
-            />
-            <input type="file" name="image" accept="image/*" hidden />
+          <input
+            type="text"
+            name="postInput"
+            placeholder="Start writing a post..."
+            className="flex-1 outline-none rounded-full py-3 px-4 border"
+          />
+          <input
+            ref={fileInputRef}
+            type="file"
+            name="image"
+            accept="image/*"
+            hidden
+            onChange={handleImageChange}
+          />
 
-            <button type="submit" hidden>
-              Post
-            </button>
+          <button type="submit" hidden>
+            Post
+          </button>
+        </div>
+
+        {/* Preview conditonal check*/}
+
+        {preview && (
+          <div className="mt-3">
+            <img src={preview} alt="preview" className="w-full object cover" />
           </div>
+        )}
+        <div className="flex justify-end mt-2 space-x-2">
+          <Button type="button" onClick={() => fileInputRef.current?.click()}>
+            <ImageIcon className="mr-2" size={16} color="currentColor" />
+            {preview ? "Change" : "Add"} Image
+          </Button>
 
-          {/* Preview conditonal check*/}
-          <div>
-            <Button>
-              <ImageIcon className="mr-2" size={16} color="currentColor" />
-              Add
+          {/* add a remove preview button */}
+
+          {preview && (
+            <Button
+              variant="outline"
+              type="button"
+              onClick={() => setPreview(null)}
+            >
+              <XIcon className="mr-2" size={16} color="currentColor" />
+              Remove Image
             </Button>
+          )}
+        </div>
+      </form>
 
-            {/* add a remove preview button */}
-          </div>
-        </form>
-      </div>
+      <hr className="mt-2 border-gray-300" />
     </div>
   );
 }
